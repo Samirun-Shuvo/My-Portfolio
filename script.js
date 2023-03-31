e9f8adf28321574d82e28facb8a6ff797c889651
Samirun-Shuvo <samirunshuvo@gmail.com>
2023-04-01 00:01:37 +0600

    emailjs update contact form

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
@@ -7,14 +7,14 @@ $(document).ready(function() {
         }
         if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up-script
     $('.scroll-up-btn').click(function () {
-       $('html').animate({scrollTop: 0}); 
+        $('html').animate({ scrollTop: 0 });
     });
 
     // toggle/menu/navbar script
@@ -25,39 +25,60 @@ $(document).ready(function() {
 
 
     //typing animation script
-    var typed = new Typed(".typing",{
-        strings: ["Web-Designer","YouTuber","Blogger","Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
+    var typed = new Typed(".typing", {
+        strings: ["Web-Designer", "YouTuber", "Blogger", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
-    var typed = new Typed(".typing-2",{
-        strings: ["Web-Designer","YouTuber","Blogger","Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
+    var typed = new Typed(".typing-2", {
+        strings: ["Web-Designer", "YouTuber", "Blogger", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
 
     // owl carousel script
     $('.carousel').owlCarousel({
-        margin:20,
+        margin: 20,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
-                items:3,
+            1000: {
+                items: 3,
                 nav: false
             }
         }
     });
+
+
+
 });
+document.getElementById("fromSubmit").addEventListener('click', function (e) {
+    e.preventDefault()
+    const contactsParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        message: document.getElementById('message').value,
+    }
+    emailjs.send("service_jb96gwt", "template_p88uf3w", contactsParams).then(function (res) {
+        if (res.status === 200) {
+            alert('Your Message Send Succefully')
+        }
+    })
+    document.getElementById('name').value = ""
+    document.getElementById('email').value = ""
+    document.getElementById('phone').value = ""
+    document.getElementById('message').value = ""
+})
